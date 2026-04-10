b1932ae1e0cf1ee92cece02cc5a2a5e9487f3e67
RESHAM-KUMARI <reshamkri682001@gmail.com>
2026-04-10 16:47:31 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
-import HeroSection from '@/sections/HeroSection';
-import AboutSection from '@/sections/AboutSection';
-import ProjectsSection from '@/sections/ProjectsSection';
-import ContactSection from '@/sections/ContactSection';
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import ProjectsSection from '@/components/ProjectsSection';
+import ContactSection from '@/components/ContactSection';
 
 export default function HomePage() {
   return (
